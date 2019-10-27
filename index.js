7414d5f90191417ba50e4fa4f71d7f7ec156e0b1
gvsrohita <gvsrohita@gmail.com>
2019-10-27 14:26:27 -0500

    save to firebase database

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
 var firebase = require("firebase/app");
+require('firebase/database');
+
 const express = require('express') ;
 const fs = require('fs');
 const bodyParser = require('body-parser');
+'use strict';
+
+const request = require('request');
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyChR6FESYhEUOJcJ3TMTZ0PdjF4TSWsfpY",
@@ -17,10 +23,11 @@ const firebaseConfig = {
 
 
 
-firebase.initializeApp(firebaseConfig);
 
 const app = express();
-app.use(bodyParser.json());
+firebase.initializeApp(firebaseConfig);
+
+app.use(bodyParser.json({limit: '100mb'}));
 
 const keyFilename="./mooc-content-analysis-firebase-adminsdk-o8avo-956b774eda.json"; //replace this with api key file
 const projectId = "mooc-content-analysis" //replace with your project id
@@ -30,10 +37,8 @@ const {Storage} = require('@google-cloud/storage');
 
 const storage = new Storage({projectId, keyFilename});
 
-const fileName = "success.png";
+const fileName = "success.jpg";
 const uploadTo = 'sample/package.json';
-
-
 
 app.get('/', function(req, res){
     res.send("Hello world!");
@@ -47,21 +52,63 @@ app.post('/videos/:video_id/users/:user_id/emotions', function(req, res){
     let data = req.body.image;
     let base64image = data.split(';base64,').pop();
     // TODO: convert base64 string to image
-    fs.writeFile('success.png', base64image, {encoding: 'base64'}, function(err) {
+    fs.writeFile('success.jpg', base64image, {encoding: 'base64'}, function(err) {
         console.log('File created');
     });
 
 storage.bucket(bucketName).upload(fileName,{
     public:true,
-    metadata: {contentType: 'image/png',cacheControl: "public, max-age=300"}
+    metadata: {contentType: 'image/jpeg',cacheControl: "public, max-age=300"}
 });
 
 storage.bucket(bucketName).file(fileName).makePublic();
 
+// Replace <Subscription Key> with your valid subscription key.
+const subscriptionKey = '595980504167406f9cee21939abc1034';
 
-    res.json("See the emotions here at time:" + req.query.time + "for the image:");
-    
+// You must use the same location in your REST call as you used to get your
+// subscription keys. For example, if you got your subscription keys from
+// westus, replace "westcentralus" in the URL below with "westus".
+const uriBase = 'https://emotionanalysis.cognitiveservices.azure.com/face/v1.0/detect';
 
+const imageUrl =
+    'https://storage.googleapis.com/mooc-content-analysis.appspot.com/success.jpg';
+
+
+// Request parameters.
+const params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes':   'emotion'
+};
+
+const options = {
+    uri: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+};
+let jsonResponse;
+request.post(options, (error, response, body) => {
+  if (error) {
+    console.log('Error: ', error);
+    return;
+  }
+    jsonResponse = JSON.stringify(response);
+    console.log(imageUrl);
+  console.log('JSON Response\n');
+//   console.log(jsonResponse);
+
+var database = firebase.database();
+database.ref('users/'+ req.params.user_id).set(response.body);
+res.send(response.body);
+});
+
+ 
+   
 });
 
 app.listen(3001);
